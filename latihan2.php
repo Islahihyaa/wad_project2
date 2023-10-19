8761a8748494227908c4cc4c5379cb72a92d7652
Islahihya Muhammad Jel Shibyan <87414482+Islahihyaa@users.noreply.github.com>
2023-10-19 15:59:03 +0700

    Create first project php html

---
+++ latihan2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2 PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Latihan 2 PHP</h2>
+                        <form action="latihan3.php" method="get">
+                            <div class="mb-3">
+                                <label for="input_angka" class="form-label">Number input</label>
+                                <input type="number" name="input_angka" id="input_angka" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="input_dropdown" class="form-label">Select an option</label>
+                                <select class="form-select" name="input_dropdown" id="input_dropdown">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Hitung</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ latihan3.php
@@ -0,0 +1,5 @@
+<?php
+    echo "Welcome to page LATIHAN 3 <br> "; 
+
+    echo $_GET['input_angka'];
+?>
